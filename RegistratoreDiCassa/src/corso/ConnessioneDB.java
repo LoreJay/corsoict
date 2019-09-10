2ac019c1b98c1a56317df31d8f98f4f064e2e2d6
Lorenzo Cioni <Lorenzo Cioni@DESKTOP-J6D9I9R>
2019-09-10 15:38:16 +0200

    Aggiunti i metodi per accedere alle tabelle

--- RegistratoreDiCassa/src/corso/ConnessioneDB.java
+++ RegistratoreDiCassa/src/corso/ConnessioneDB.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import internalconnection.Product;
 
@@ -16,14 +17,16 @@ public class ConnessioneDB {
 	private PreparedStatement stmt_addProduct = null;
 	private PreparedStatement stmt_editQta = null;
 	private PreparedStatement stmt_creaScontrino = null;
+	private PreparedStatement stmt_getCodice = null;
 	private PreparedStatement stmt_editTotale = null;
-	private PreparedStatement[] allStmt = {stmt_getProduct, stmt_addProduct, stmt_editQta, stmt_creaScontrino, stmt_editTotale}; 
+	private PreparedStatement[] allStmt = {stmt_getProduct, stmt_addProduct, stmt_editQta, stmt_creaScontrino, stmt_getCodice, stmt_editTotale}; 
 	
-	private final static String QUERY_GET_PRODUCT = "SELECT * FROM prodotti WHERE prodotti.codice = (?)";
+	private final static String QUERY_GET_PRODUCT = "SELECT * FROM prodotti";
 	private final static String QUERY_ADD_PRODUCT = "INSERT INTO prodotti_scontrini VALUES (?, ?, ?, ?)";
-	private final static String QUERY_EDT_QTA = "UPDATE prodotti_scontrini VALUSE qta = (?) WHERE Codice_prodotto = (?)";
-	private final static String QUERY_NEW_SCONTRINO = "INSERT INTO scontrini VALUES (?, 0, ?, ?)";
-	private final static String QUERY_EDT_TOT = "UPDATE scontrini VALUES totale = (?) WHERE ID = (?)";
+	private final static String QUERY_EDT_QTA = "UPDATE prodotti_scontrini SET qta = (?) WHERE Codice_prodotto = (?)";
+	private final static String QUERY_NEW_SCONTRINO = "INSERT INTO scontrini (iva) VALUES (?)";
+	private final static String QUERY_GET_ID_SCONTRINO = "SELECT id FROM scontrini ORDER BY DataOra DESC LIMIT 1";
+	private final static String QUERY_EDT_TOT = "UPDATE scontrini SET totale = (?) WHERE ID = (?)";
 	
 	public ConnessioneDB() throws SQLException {
 		conn = getConnection();
@@ -48,6 +51,7 @@ public class ConnessioneDB {
 		stmt_addProduct = conn.prepareStatement(QUERY_ADD_PRODUCT);
 		stmt_editQta = conn.prepareStatement(QUERY_EDT_QTA);
 		stmt_creaScontrino = conn.prepareStatement(QUERY_NEW_SCONTRINO);
+		stmt_getCodice = conn.prepareStatement(QUERY_GET_ID_SCONTRINO);
 		stmt_editTotale = conn.prepareStatement(QUERY_EDT_TOT);
 	}
 	
@@ -64,13 +68,22 @@ public class ConnessioneDB {
 		}
 	}
 	
-	public Product cercaProdotto (String codice) throws SQLException {
+	/**
+	 * Ottieni una ArrayList di Product presentei nel database
+	 * 
+	 * @param codice del prodotto
+	 * @return Product trovato
+	 * @throws SQLException
+	 */
+	
+	public ArrayList<Product> listaProdotti () throws SQLException {
 		
-		Product aux = null;
-		this.stmt_getProduct.setString(1, codice);
+		ArrayList<Product> aux = new ArrayList<>();
 		
 		try (ResultSet rs = this.stmt_getProduct.executeQuery()){
-			aux = new Product (rs.getString("codice"), rs.getString("nome"), rs.getFloat("prezzo"));
+			while(rs.next()) {
+			aux.add(new Product (rs.getString("codice"), rs.getString("nome"), rs.getFloat("prezzo")));
+			}
 		} catch (SQLException sqle) {
 			System.out.println("Errore nelle QUERY_GET_PRODUCT");
 			sqle.printStackTrace();
@@ -81,11 +94,27 @@ public class ConnessioneDB {
 	
 	/**
 	 * Aggiungi prodotto a prodotti_scontrini
+	 * @throws SQLException 
 	 */
 	
-	public void aggiungiProdotto (int codScontrino, String codiceProdotto, int qta, float prezzo_applicato) {
+	public void aggiungiProdotto (int codScontrino, String codiceProdotto, int qta, float prezzo_applicato) throws SQLException {
 		
+		this.stmt_addProduct.setInt(1, codScontrino);
+		this.stmt_addProduct.setString(2, codiceProdotto);
+		this.stmt_addProduct.setInt(3, qta);
+		this.stmt_addProduct.setFloat(4, prezzo_applicato);
+		
+		this.stmt_addProduct.executeUpdate();
 	}
+	
+	/**
+	 * Aggiorna la quantità del prodotto scelto
+	 * 
+	 * @param codice del prodotto
+	 * @param newQta nuova quantità
+	 * @throws SQLException
+	 */
+	
 	
 	public void aggiornaQta (String codice, int newQta) throws SQLException {
 		
@@ -95,12 +124,47 @@ public class ConnessioneDB {
 		this.stmt_editQta.executeUpdate();
 	}
 	
-	public void creaScontrino (int codice, float iva) throws SQLException {
+	/**
+	 * Crea un istanza di scontrino nella tabella scontrini
+	 * 
+	 * @param iva
+	 * @throws SQLException
+	 * @return codice dello scontrino appena creato
+	 */
+	
+	public int creaScontrino (float iva) throws SQLException {
 		
-		this.stmt_creaScontrino.setInt(1, codice);
-		this.stmt_creaScontrino.setFloat(3, iva);
+	
+		this.stmt_creaScontrino.setFloat(1, iva);
 		
 		this.stmt_creaScontrino.executeUpdate();
+		
+		int cod = 0;
+		
+		//Cerca il codice dello scontrino appena creato
+		try (ResultSet rs = this.stmt_getCodice.executeQuery()){
+			cod = rs.getInt(1);
+		} catch (SQLException sqle) {
+			//
+		}
+		
+		return cod;
+	}
+	
+	/**
+	 * Aggiorna il conto totale dello scontrino selezionato
+	 * 
+	 * @param prezzoNuovo 
+	 * @param idScontrino 
+	 * @throws SQLException
+	 */
+	
+	public void aggionraTot (float prezzoNuovo, int idScontrino) throws SQLException {
+		
+		this.stmt_editTotale.setFloat(1, prezzoNuovo);
+		this.stmt_editTotale.setInt(2, idScontrino);
+		
+		this.stmt_editTotale.executeUpdate();
 	}
 	
 
